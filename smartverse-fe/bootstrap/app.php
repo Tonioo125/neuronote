c5de29b7885debd8fa0f9f437b0841b2026782be
Antonio Owen Putra Amadeus <143788026+Tonioo125@users.noreply.github.com>
2026-06-09 00:56:47 +0700

    Set custom storage path for application

--- smartverse-fe/bootstrap/app.php
+++ smartverse-fe/bootstrap/app.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+$storagePath = '/tmp/storage';
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -15,4 +17,6 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create()
+    ->useStoragePath($storagePath);
