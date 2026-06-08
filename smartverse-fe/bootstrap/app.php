ed8dfcf8b01c385c72ac3cd947c94efc7aea8f9f
Antonio Owen Putra Amadeus <143788026+Tonioo125@users.noreply.github.com>
2026-06-09 01:16:51 +0700

    Create necessary directories for application storage

--- smartverse-fe/bootstrap/app.php
+++ smartverse-fe/bootstrap/app.php
@@ -5,6 +5,15 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 $storagePath = '/tmp/storage';
+foreach ([
+    $storagePath . '/framework/cache/data',
+    $storagePath . '/framework/sessions',
+    $storagePath . '/framework/views',
+    $storagePath . '/logs',
+    $storagePath . '/app/public',
+] as $dir) {
+    if (!is_dir($dir)) mkdir($dir, 0755, true);
+}
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,7 +25,19 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (\Throwable $e, $request) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'file' => str_replace('/var/task/user/', '', $e->getFile()),
+                'line' => $e->getLine(),
+                'class' => get_class($e),
+                'trace' => collect($e->getTrace())->take(5)->map(fn($t) => [
+                    'file' => str_replace('/var/task/user/', '', $t['file'] ?? ''),
+                    'line' => $t['line'] ?? '',
+                    'function' => $t['function'] ?? '',
+                ])->toArray(),
+            ], 500);
+        });
     })
     ->create()
     ->useStoragePath($storagePath);
